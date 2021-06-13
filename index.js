f13fe0c6261ac8150586ec582ae7b1284218b19c
andika.pelealu90 <andika.pelealu90@gmail.com>
2021-06-13 21:56:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require ('dotenv').config()
+const cors = require('cors')
+const express = require ('express')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req,res)=> {
+    res.redirect('https://www.yahoo.com')
+})
+const PORT = Number(process.env.PORT) || 3000
+
+app.listen(PORT, () => {console.log(` app running on port ${PORT} `)})
